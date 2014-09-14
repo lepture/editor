8efcaa3929edb3226774ef269062375d4b9df27d
Bhargav Anand <rjn143@gmail.com>
2014-09-13 22:05:24 -0400

    fixed multiple words list bug

--- vendor/continuelist.js
+++ vendor/continuelist.js
@@ -1,23 +1,33 @@
 (function() {
   'use strict';
 
-  var listRE = /^(\s*)([*+-]|(\d+)\.)(\s*)/,
+  var listRE = /^(\s*)([*+-]|(\d+)\.)([\w+(\s+\w+)]|[\s*])/,
+      emptyListRE = /^(\s*)([*+-]|(\d+)\.)(\s*)$/,
       unorderedBullets = '*+-';
 
   CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
-    var pos = cm.getCursor(),
-        inList = cm.getStateAfter(pos.line).list,
+    var pos = cm.getCursor(), tok,
+        inList = cm.getStateAfter(pos.line).list || !cm.getStateAfter(pos.line).list,
         match;
+
+    var emptyMatch = cm.getLine(pos.line).match(emptyListRE);
+
+    if (!inList || emptyMatch){
+      tok = cm.getTokenAt(pos);
+      cm.replaceRange("", {line: pos.line , ch:tok.start}, {line:pos.line , ch:tok.end});
+      cm.execCommand('newlineAndIndent');
+      return
+    }
 
     if (!inList || !(match = cm.getLine(pos.line).match(listRE))) {
       cm.execCommand('newlineAndIndent');
       return;
     }
 
-    var indent = match[1], after = match[4];
+    var indent = match[1], after = " ";
     var bullet = unorderedBullets.indexOf(match[2]) >= 0
-      ? match[2]
-      : (parseInt(match[3], 10) + 1) + '.';
+        ? match[2]
+        : (parseInt(match[3], 10) + 1) + '.';
 
     cm.replaceSelection('\n' + indent + bullet + after, 'end');
   };
